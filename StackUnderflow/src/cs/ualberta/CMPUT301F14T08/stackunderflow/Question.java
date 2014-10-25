bc8b0b6a86f813ce1618c1ddc70587011a83f341
Tamara <tamarabyte@gmail.com>
2014-10-25 14:03:54 -0600

    Added default Objects from Bens Branch

--- StackUnderflow/src/cs/ualberta/CMPUT301F14T08/stackunderflow/Question.java
+++ StackUnderflow/src/cs/ualberta/CMPUT301F14T08/stackunderflow/Question.java
@@ -3,53 +3,40 @@ package cs.ualberta.CMPUT301F14T08.stackunderflow;
 import java.util.ArrayList;
 
 /**
- * 
  * StackUnderflow application
  * Question class, extends Post class
- * @author Michael Williams
- * 
  */
 
 
 public class Question extends Post {
 	
-	private boolean mIsFavorite;
-	private boolean mIsRead;
-	
-	private ArrayList<Answer> answers;
+	private String mTitle;
+	private ArrayList<Answer> mAnswers;
 
-	public Question() {
-		super();
-		answers = new ArrayList<Answer>();
+	public Question(String text, String signature, String title){
+		this(text, signature, null, title);
+	}
+	
+	public Question(String text, String signature, String picture, String title){
+		super(text, signature, picture);
+		mTitle = title;	
+	}
+	
+	public String getTitle(){
+		return mTitle;
 	}
 
 	public ArrayList<Answer> getAnswers() {
-		return answers;
+		return mAnswers;
 	}
 
-	public void setAnswers(ArrayList<Answer> answers) {
-		this.answers = answers;
+	public int countAnswers(){
+		return mAnswers.size();
 	}
 	
-	public void addAnswer(Answer answer){
-		answers.add(answer);
+	public void addAnswer(Answer newAnswer){
+		newAnswer.setParentQuestion(this);
+		mAnswers.add(newAnswer);
 	}
-
-	public boolean getIsFavorite() {
-		return mIsFavorite;
-	}
-
-	public void setIsFavorite(boolean mIsFavorite) {
-		this.mIsFavorite = mIsFavorite;
-	}
-
-	public boolean getmIsRead() {
-		return mIsRead;
-	}
-
-	public void setmIsRead(boolean mIsRead) {
-		this.mIsRead = mIsRead;
-	}
-	
 
 }
---
+++ StackUnderflow/src/cs/ualberta/CMPUT301F14T08/stackunderflow/UserAttributes.java
@@ -0,0 +1,71 @@
+package cs.ualberta.CMPUT301F14T08.stackunderflow;
+
+public class UserAttributes {
+	
+	private boolean mIsUpvoted;
+	private boolean mIsFavorited;
+	private boolean mIsReadLater;
+	private boolean mIsUsers;
+	private boolean mIsRead;
+	private boolean mIsPushedToLive;
+	
+	public UserAttributes(){
+		this(false);
+	}
+	
+	public UserAttributes(boolean isUsers){
+		mIsUsers = isUsers;
+		mIsUpvoted = false;
+		mIsFavorited = false;
+		mIsReadLater = false;
+		mIsRead = false;
+		mIsPushedToLive = false;
+	}
+
+	public boolean getIsFavorited() {
+		return mIsFavorited;
+	}
+
+	public void toggleIsFavorited() {
+		mIsFavorited = !mIsFavorited;
+	}
+
+	public boolean getIsUpvoted() {
+		return mIsUpvoted;
+	}
+
+	public void toggleIsUpvoted() {
+		mIsUpvoted = !mIsUpvoted;
+	}
+	
+	public boolean getIsUsers() {
+		return mIsUsers;
+	}
+
+	public boolean getIsReadLater() {
+		return mIsReadLater;
+	}
+
+	public void setIsReadLater(boolean isReadLater) {
+		mIsReadLater = isReadLater;
+	}
+
+	public boolean getIsRead() {
+		return mIsRead;
+	}
+
+	public void setIsReadTrue() {
+		mIsRead = true;
+		if (mIsReadLater) setIsReadLater(false);
+	}
+
+	public boolean getIsPushedToLive() {
+		return mIsPushedToLive;
+	}
+
+	public void setIsPushedToLive(boolean isPushedToLive) {
+		mIsPushedToLive = isPushedToLive;
+	}
+
+	
+}

4f426579bdc5fab1e2894a3262ef2fd909cedc70
Benjamin Lavin <blavin@ualberta.ca>
2014-10-27 14:55:49 -0600

    Removed bin and gen

--- StackUnderflow/gen/cs/ualberta/CMPUT301F14T08/stackunderflow/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package cs.ualberta.CMPUT301F14T08.stackunderflow;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}

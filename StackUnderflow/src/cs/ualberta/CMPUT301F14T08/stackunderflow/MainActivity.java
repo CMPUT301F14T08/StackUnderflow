55d3a9dd91407aacdc0da163895cb3bdf29c253b
mswillia <mswillia@ualberta.ca>
2014-11-04 18:15:09 -0700

    New Question basic layout.xml, class, intent passing

--- StackUnderflow/src/cs/ualberta/CMPUT301F14T08/stackunderflow/MainActivity.java
+++ StackUnderflow/src/cs/ualberta/CMPUT301F14T08/stackunderflow/MainActivity.java
@@ -21,10 +21,11 @@ import android.widget.ListView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 
 
-public class MainActivity extends Activity implements TabListener {// implements ActionBar.TabListener{//BaseFragmentActivity implements ActionBar.TabListener {
+public class MainActivity extends Activity implements TabListener {
 	
 	/*
     @Override
@@ -34,10 +35,8 @@ public class MainActivity extends Activity implements TabListener {// implements
 	}
 	*/
 	
-	private PostController sPostController;
-	private PostAdapter adapter;
 	List<Fragment> fragmentList = new ArrayList<Fragment>();
-	
+	int temp;
 	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);	
@@ -61,25 +60,46 @@ public class MainActivity extends Activity implements TabListener {// implements
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {//, MenuInflater inflater) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		//super.onCreateOptionsMenu(menu, inflater);
+
 		getMenuInflater().inflate(R.menu.fragment_main_menu, menu);
 		return true;
 	}
+	
+	
 	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
+
+		/*
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
 			return true;
 		}
+		*/
+		
+	    switch (item.getItemId()) {
+			case R.id.ask_question:			
+
+				Intent intent = new Intent(this, NewQuestionActivity.class);
+				startActivity(intent);
+			    return true;
+			default:
 				
-		//return super.onOptionsItemSelected(item);
-		return onOptionsItemSelected(item);
-	}
+				CharSequence text = "Implement menu item";
+				int duration = Toast.LENGTH_SHORT;
+				Toast toast = Toast.makeText(this, text, duration);
+				toast.show();
+				
+				//return onOptionsItemSelected(item);
+				
+				return false;
+	    } 
 	
+
+	}	
 	
 	/*
 	@Override

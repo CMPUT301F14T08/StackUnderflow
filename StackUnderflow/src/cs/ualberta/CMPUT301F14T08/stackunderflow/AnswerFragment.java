020ed0d422858cd070b2531a0eb99372d7657a6c
Tamarabyte <tbain@ualberta.ca>
2014-11-08 02:28:19 -0700

    Changed Layout of Post Fragments. They are much prettier now. Put shared logic from AnswerFragment/QuestionFragment in Superclass.

--- StackUnderflow/src/cs/ualberta/CMPUT301F14T08/stackunderflow/AnswerFragment.java
+++ StackUnderflow/src/cs/ualberta/CMPUT301F14T08/stackunderflow/AnswerFragment.java
@@ -4,19 +4,15 @@
  */
 package cs.ualberta.CMPUT301F14T08.stackunderflow;
 
-import java.text.SimpleDateFormat;
-import java.util.Locale;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class AnswerFragment extends PostFragment {
 	
@@ -31,127 +27,67 @@ public class AnswerFragment extends PostFragment {
 		mParent = (Question)sPostController.getPostManager().getPost(mAnswer.getParentID());
 	}
 	
-	@Override
-	public void onPause(){
-		super.onPause();
-	}
-	
+   @Override
+    protected int getUpvoteFullID() {
+        return R.drawable.upvote_full_blue;
+    }
+    
+    @Override
+    protected int getUpvoteEmptyID() {
+        return R.drawable.upvote_empty_blue;
+    }
+    
+    @Override
+    protected int getFavoriteFullID() {
+        return R.drawable.star_full_blue;
+    }
+    
+    @Override
+    protected int getFavoriteEmptyID() {
+        return R.drawable.star_empty_blue;
+    }
+    
+    @Override
+    protected int getArrowLeftID() {
+        return R.drawable.box_arrow_left_blue;
+    }
+    
+    @Override
+    protected int getArrowRightID() {
+        return R.drawable.box_arrow_right_blue;
+    }
+    
+    @Override
+    protected int getTextColor() {
+        return R.color.blue;
+    }
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 	    switch (item.getItemId()) {
-			case R.id.menu_item_new_answer:
-				
-				Intent intent = new Intent(getActivity(), NewAnswerActivity.class);
-				intent.putExtra(PostFragment.EXTRA_POST_ID, mAnswer.getParentID()); 
-				startActivity(intent);
-				
-//				if(sPostController.getPostManager().getUserProfileManager().getUserProfile().getUsername.equals(null)){
-//	        		FragmentManager fm = getActivity().getFragmentManager();
-//					UsernameFragment dialog = new UsernameFragment();
-//					dialog.setTargetFragment(AnswerFragment.this, REQUEST_USERNAME);
-//					dialog.show(fm, DIALOG_USERNAME);
-//				}
-//				else{
-//					Intent i = new Intent(getActivity(), NewAnswerActivity.class);
-//					i.putExtra(NewAnswerFragment.EXTRA_PARENT_QUESTION_ID, mAnswer.getParentQuestion().getID());
-//					startActivity(i);
-//				}
-			    return true;
 			case R.id.menu_item_back_to_question:
 				Intent i = new Intent(getActivity(), QuestionActivity.class);
 				i.putExtra(PostFragment.EXTRA_POST_ID, mAnswer.getParentID());
 				startActivity(i);				 
 			    return true;
 			default:
+			    // Call PostFragment onOptionItemSelected to get the rest of the menu
 				return super.onOptionsItemSelected(item);
 	    } }
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState){
-		View v = inflater.inflate(R.layout.post_fragment, parent, false);
+	    // Call PostFragment onCreateView
+		View v = super.onCreateView(inflater, parent, savedInstanceState);
 		
+		// Logic unique to the Answer Fragment
 		LinearLayout linearLayout = (LinearLayout)v.findViewById(R.id.post_fragment_top_linearlayout);
-		linearLayout.setBackgroundColor(mBlueColor);
+		linearLayout.setBackgroundColor(mTextColor);
 		
 		mQuestionTitle = (TextView)v.findViewById(R.id.post_fragment_textview_title);
 		mQuestionTitle.setVisibility(View.GONE);
 		
-		mPostBody = (TextView)v.findViewById(R.id.post_fragment_textview_body);
-		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy", Locale.CANADA);
-		String date = sdf.format(mAnswer.getDate());
-		mPostBody.setText(mAnswer.getText() + " (" + date + ")");
-		mPostBody.setTextColor(mWhiteColor);
-		
-		mUpvoteButton = (ImageButton)v.findViewById(R.id.post_fragment_button_upvote);
-		mUpvoteButton.setImageResource(mAnswer.getUserAttributes().getIsUpvoted() ? R.drawable.upvote_full : R.drawable.upvote_empty);
-		mUpvoteButton.setBackgroundColor(mBlueColor);
-		mUpvoteButton.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				mAnswer.getUserAttributes().toggleIsUpvoted();
-				if(mAnswer.getUserAttributes().getIsUpvoted()){
-					mAnswer.incrementVotes();
-					mUpvoteButton.setImageResource(R.drawable.upvote_full);
-				}
-				else{
-					mAnswer.decrementVotes();
-					mUpvoteButton.setImageResource(R.drawable.upvote_empty);
-				}		
-				mUpvoteCountTextView.setText(""+mAnswer.getVotes());
-			}
-		});
-		
-		
-		mUpvoteCountTextView = (TextView)v.findViewById(R.id.post_fragment_textview_upvotes);
-		mUpvoteCountTextView.setText(""+ mAnswer.getVotes());
-		mUpvoteCountTextView.setTextColor(mWhiteColor);
-		
-		mFavoriteButton = (ImageButton)v.findViewById(R.id.post_fragment_button_favorite);
-		mFavoriteButton.setImageResource(mAnswer.getUserAttributes().getIsFavorited() ? R.drawable.star_full : R.drawable.star_empty);
-		mFavoriteButton.setBackgroundColor(mBlueColor);
-		mFavoriteButton.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				mAnswer.getUserAttributes().toggleIsFavorited();
-				mFavoriteButton.setImageResource(mAnswer.getUserAttributes().getIsFavorited() ? R.drawable.star_full : R.drawable.star_empty);
-			}
-		});
-		
-		mFavoriteTextView = (TextView)v.findViewById(R.id.post_fragment_textview_favorite);
-		mFavoriteTextView.setTextColor(mWhiteColor);
-		
-		mPictureButton = (ImageButton)v.findViewById(R.id.post_fragment_button_photo);
-		if(mAnswer.hasPicture()){
-			mPictureButton.setImageResource(R.drawable.picture_white);
-			mPictureButton.setEnabled(true);
-			mPictureButton.setVisibility(View.VISIBLE);
-			mPictureButton.setBackgroundColor(mBlueColor);
-			mPictureButton.setOnClickListener(new View.OnClickListener() {
-				
-				@Override
-				public void onClick(View v) {
-					// Show the picture
-					String toastString = "Someone needs to implement code to show the picture";
-	                Toast toast = Toast.makeText(getActivity().getApplicationContext(), toastString, Toast.LENGTH_LONG);
-	                toast.show();    
-				}
-			});
-		}
-		else{
-			mPictureButton.setImageResource(R.drawable.picture_dark);
-			mPictureButton.setEnabled(false);
-			mPictureButton.setVisibility(View.GONE);
-		}
-		
-		
-		mUsername = (TextView)v.findViewById(R.id.post_fragment_textview_username);
-		mUsername.setText(mAnswer.getSignature());
-		mUsername.setTextColor(mWhiteColor);
-		
-		
-		mAnswersButton = (ImageButton)v.findViewById(R.id.post_fragment_button_answers);
+		mAnswersButton = (Button)v.findViewById(R.id.post_fragment_button_answers);
 		
 		final int position = sPostController.getPostManager().getPositionOfAnswer(mParent, mAnswer);
 		int remainingAnswers = mParent.countAnswers() - position - 1;
@@ -159,7 +95,8 @@ public class AnswerFragment extends PostFragment {
 			
 			mAnswersButton.setEnabled(true);
 			mAnswersButton.setVisibility(View.VISIBLE);
-			mAnswersButton.setImageResource(R.drawable.box_arrow_right_large);
+			mAnswersButton.setText(remainingAnswers + " More ");
+
 			mAnswersButton.setOnClickListener(new View.OnClickListener() {
 				public void onClick(View v) {
 					Intent i = new Intent(getActivity(), AnswerActivity.class);
@@ -171,20 +108,6 @@ public class AnswerFragment extends PostFragment {
 		else{
 			mAnswersButton.setEnabled(false);
 			mAnswersButton.setVisibility(View.GONE);
-		}
-		
-		mAnswersTextView = (TextView)v.findViewById(R.id.post_fragment_textview_answers);
-		mAnswersTextView.setTextColor(mBlackColor);
-		switch(remainingAnswers){
-		case 0:
-			mAnswersTextView.setText("No More Answers");
-			break;
-		case 1:
-			mAnswersTextView.setText("1 More Answer");
-			break;
-		default:
-			mAnswersTextView.setText(remainingAnswers + " More Answers");
-			break;
 		}
 		
 		return v;		

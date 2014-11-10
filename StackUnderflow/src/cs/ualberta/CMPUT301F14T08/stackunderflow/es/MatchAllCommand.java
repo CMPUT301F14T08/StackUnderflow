ae5f2143478e08a44956e1b42e7c0af343a19ee6
MaciejOgrocki <ogrocki@ualberta.ca>
2014-11-10 14:17:34 -0700

    Added Java doc sorces for the all the ES information

--- StackUnderflow/src/cs/ualberta/CMPUT301F14T08/stackunderflow/es/MatchAllCommand.java
+++ StackUnderflow/src/cs/ualberta/CMPUT301F14T08/stackunderflow/es/MatchAllCommand.java
@@ -1,5 +1,7 @@
+/**
+ * Taken from https://github.com/dfserrano/AndroidElasticSearch/ used to add and remove information from elastic search online.
+ */
 package cs.ualberta.CMPUT301F14T08.stackunderflow.es;
-
 
 public class MatchAllCommand implements ElasticSearchCommand {
 
--- StackUnderflow/src/cs/ualberta/CMPUT301F14T08/stackunderflow/es/SearchHits.java
+++ StackUnderflow/src/cs/ualberta/CMPUT301F14T08/stackunderflow/es/SearchHits.java
@@ -1,3 +1,6 @@
+/**
+ * Taken from https://github.com/dfserrano/AndroidElasticSearch/ used to add and remove information from elastic search online.
+ */
 package cs.ualberta.CMPUT301F14T08.stackunderflow.es;
 
 import java.util.List;

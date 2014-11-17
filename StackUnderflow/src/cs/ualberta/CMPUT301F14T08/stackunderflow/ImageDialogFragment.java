509118f05b85ccb2130da8f80b4a75a7122fea8c
Ross <rcockbur@ualberta.ca>
2014-11-16 17:35:11 -0700

    got compression working

--- StackUnderflow/src/cs/ualberta/CMPUT301F14T08/stackunderflow/ImageDialogFragment.java
+++ StackUnderflow/src/cs/ualberta/CMPUT301F14T08/stackunderflow/ImageDialogFragment.java
@@ -3,6 +3,9 @@ package cs.ualberta.CMPUT301F14T08.stackunderflow;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -25,7 +28,7 @@ import android.widget.ImageButton;
 
 public class ImageDialogFragment extends DialogFragment {
 	
-	Uri imageFileUri;
+	protected Uri mImageFileUri;
 	protected Drawable mImage;
 	protected Bitmap mBitmap;
 	protected Drawable mDrawable;
@@ -75,32 +78,51 @@ public class ImageDialogFragment extends DialogFragment {
 			folder.mkdir();
 		String imagePathAndFileName = path + File.separator + String.valueOf(System.currentTimeMillis()) + ".jpg";
 		File imageFile = new File(imagePathAndFileName);
-		imageFileUri = Uri.fromFile(imageFile);
+		mImageFileUri = Uri.fromFile(imageFile);
 		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-		intent.putExtra(MediaStore.EXTRA_OUTPUT, imageFileUri);
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageFileUri);
 		startActivityForResult(intent, CAPTURE_IMAGE_REQUEST_CODE);
 	}
 	
+	//jpeg object is loaded into memory, then scaled down by a factor of two until
+	// file size is below 64kb. Scaling is donw by converting JPEG to bitmap at half size,
+	//then converted back to JPEG
 	public void onActivityResult(int requestCode, int resultCode, Intent data){
 		if (requestCode == CAPTURE_IMAGE_REQUEST_CODE) {
 			if (resultCode == Activity.RESULT_OK) {
-				Bitmap original = BitmapFactory.decodeFile(imageFileUri.getPath());
-				ByteArrayOutputStream out = new ByteArrayOutputStream();
-				original.compress(Bitmap.CompressFormat.JPEG, 70, out);
-				Bitmap modified = BitmapFactory.decodeStream(new ByteArrayInputStream(out.toByteArray()));
+				String imageFilePath = mImageFileUri.getPath();
+				File imageFile = new File(imageFilePath);					//loads JPEG to memory
+				BitmapFactory.Options o = new BitmapFactory.Options();
+				o.inSampleSize = 2;											//Factor for scaling down
+				Bitmap modified = null;										
+				FileOutputStream fos = null;
+				long fileSize = imageFile.length();
+				//Log.d("MYTAGE", String.valueOf(fileSize), new Exception());
 				
-				//debug stuff. the log shows that original and modified are the same size.
-				//I suspect this is because both are bitmaps versions.
-				//Not sure how to measure the jpegs directly, but i expect they are different sizes.
-				int bytesOriginal = original.getByteCount();
-				Log.d("MYTAG", String.valueOf(bytesOriginal), new Exception());
-				int bytesModified = modified.getByteCount();
-				Log.d("MYTAG", String.valueOf(bytesModified), new Exception());
+				//each iteration shrinks image by a factor of two
+				while(fileSize > 64000){
+					try {
+						modified = BitmapFactory.decodeStream(new FileInputStream(imageFile), null, o); //converts JPEG to bitmap at half size
+						fos = new FileOutputStream(imageFile);
+					} 
+					catch (FileNotFoundException e) {
+						e.printStackTrace();
+					}
+					modified.compress(Bitmap.CompressFormat.JPEG, 100, fos);	//converts bitmap back to JPEG
+					fileSize = imageFile.length();
+					//Log.d("MYTAGE", String.valueOf(fileSize), new Exception());
+				}
 				
-				mDrawable = new BitmapDrawable(getResources(), modified);
-				mImageButton.setImageDrawable(mDrawable);
-				
-			}
+				//converts JPEG to bitmap without scaling, so that it can be drawn.
+				try {
+					modified = BitmapFactory.decodeStream(new FileInputStream(imageFile));
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				}
+
+				mDrawable = new BitmapDrawable(getResources(), modified); 	//creates drawable from bitmap
+				mImageButton.setImageDrawable(mDrawable);					//updates thumbnail view
+				}
 		}
 	}
 }

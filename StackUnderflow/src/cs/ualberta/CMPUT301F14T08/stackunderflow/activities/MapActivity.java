b9c20425201048c019ed0327000640f42dc9f9b3
Benjamin Lavin <blavin@ualberta.ca>
2014-11-29 03:38:48 -0700

    Refactoring & UI changes to location and image fragments. Restrict activities to portait mode

--- StackUnderflow/src/cs/ualberta/CMPUT301F14T08/stackunderflow/activities/MapActivity.java
+++ StackUnderflow/src/cs/ualberta/CMPUT301F14T08/stackunderflow/activities/MapActivity.java
@@ -5,8 +5,11 @@ import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
@@ -23,90 +26,84 @@ import cs.ualberta.CMPUT301F14T08.stackunderflow.managers.LocManager;
  */
 public class MapActivity extends Activity {
 
-	public GoogleMap googleMap = null;
-	public Marker m = null;
+	private GoogleMap mGoogleMap = null;
+	private Marker mMarker = null;
+	private Button mMarkerButton;
+	private Button mGPSButton;
+	
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.map_fragment);
+		final Activity activity = this;
 
-		googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
-		googleMap.setMyLocationEnabled(true);
-		googleMap.getMyLocation();
-		googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+		mGoogleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+		mGoogleMap.setMyLocationEnabled(true);
+		mGoogleMap.getMyLocation();
+		mGoogleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
 
 			@Override
 			public void onMapClick(LatLng position) {
-				m=null;
-				googleMap.clear();
-				m = googleMap.addMarker(new MarkerOptions().position(position));
-				Log.d("winrar", ""+m.getPosition());
+				mMarker=null;
+				mGoogleMap.clear();
+				mMarker = mGoogleMap.addMarker(new MarkerOptions().position(position));
+				Log.d("winrar", ""+mMarker.getPosition());
 			}
 		});
-	}
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {//, MenuInflater inflater) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.map_menu, menu);
-
-		return true;
-	}
-	public boolean onOptionsItemSelected(MenuItem item) {
-
-		int duration = Toast.LENGTH_LONG;
-		CharSequence text = "";
-		Toast toast = null;
-		switch (item.getItemId()) {
-		case R.id.my_location: 
-			Location myLocation = googleMap.getMyLocation();
-			if(myLocation == null){
-				toast = Toast.makeText(this, "We cannot find your GPS location", duration);
-				toast.show();
-			}
-			else{
-				Intent msg = new Intent();
-                msg.putExtra("latitude", myLocation.getLatitude());
-                msg.putExtra("longitude", myLocation.getLongitude());
-
-                setResult(Activity.RESULT_OK, msg);
-				this.finish();
-
-			}
-			return true;
-
-		case R.id.marker:
+		
+		mMarkerButton = (Button) findViewById(R.id.map_fragment_marker_button);
+		mMarkerButton.setOnClickListener(new View.OnClickListener() {
 			
-			if(m==null){
-				toast = Toast.makeText(this, "You need to set a marker", duration);
-				toast.show();
-			}
-			else{
-				double latitude = m.getPosition().latitude;
-				double longitude = m.getPosition().longitude;
-				if(LocManager.getLocationString(this, new LatLng(latitude, longitude)) != null){
-					Intent msg = new Intent();
-	                msg.putExtra("latitude", latitude);
-	                msg.putExtra("longitude", longitude);
-	
-	                setResult(Activity.RESULT_OK, msg);
-					this.finish();
+			@Override
+			public void onClick(View v) {
+				if(mMarker==null){
+					errorToast("You need to set a marker");
 				}
 				else{
-					toast = Toast.makeText(this, "No location found at given marker point", duration);
-					toast.show();
+					double latitude = mMarker.getPosition().latitude;
+					double longitude = mMarker.getPosition().longitude;
+					if(LocManager.getLocationString(activity, new LatLng(latitude, longitude)) != null){
+						Intent msg = new Intent();
+		                msg.putExtra("latitude", latitude);
+		                msg.putExtra("longitude", longitude);
+		
+		                setResult(Activity.RESULT_OK, msg);
+						activity.finish();
+					}
+					else{
+						errorToast("No location found at given marker point");
+					}
+
 				}
-
+				
 			}
-			return true;
+		});
+		
+		mGPSButton = (Button) findViewById(R.id.map_fragment_gps_button);
+		mGPSButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Location myLocation = mGoogleMap.getMyLocation();
+				if(myLocation == null){
+					errorToast("Cannot find your GPS location");
+				}
+				else{
+					Intent msg = new Intent();
+	                msg.putExtra("latitude", myLocation.getLatitude());
+	                msg.putExtra("longitude", myLocation.getLongitude());
 
-		default:				
-			text = "Implement menu item";
-			toast = Toast.makeText(this, text, duration);
-			toast.show();			
-			return false;
-		} 
+	                setResult(Activity.RESULT_OK, msg);
+					activity.finish();
+
+				}
+			}
+		});
+		
+	}
+	
+	public void errorToast(String errorText){
+		Toast toast = Toast.makeText(this, errorText, Toast.LENGTH_LONG);
+		toast.setGravity(Gravity.BOTTOM, 0, findViewById(R.id.map_fragment_buttons_layout).getHeight()+5);
+		toast.show();
 	}
 }
-
-
-
-

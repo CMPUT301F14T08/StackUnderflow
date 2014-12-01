872efd38d7754b1ee113a013fc938c310a91ed03
Benjamin Lavin <blavin@ualberta.ca>
2014-12-01 08:49:05 -0700

    Added some comments

--- StackUnderflow/src/cs/ualberta/CMPUT301F14T08/stackunderflow/controllers/OnSwipeTouchListener.java
+++ StackUnderflow/src/cs/ualberta/CMPUT301F14T08/stackunderflow/controllers/OnSwipeTouchListener.java
@@ -13,10 +13,10 @@ import android.view.View.OnTouchListener;
  */
 public class OnSwipeTouchListener implements OnTouchListener {
 
-    private final GestureDetector gestureDetector;
+    private final GestureDetector mGestureDetector;
 
     public OnSwipeTouchListener(Context context) {
-        gestureDetector = new GestureDetector(context, new GestureListener());
+    	mGestureDetector = new GestureDetector(context, new GestureListener());
     }
 
     public void onSwipeLeft() {
@@ -26,13 +26,13 @@ public class OnSwipeTouchListener implements OnTouchListener {
     }
 
     public boolean onTouch(View v, MotionEvent event) {
-        return gestureDetector.onTouchEvent(event);
+        return mGestureDetector.onTouchEvent(event);
     }
 
     private final class GestureListener extends SimpleOnGestureListener {
 
-        private static final int SWIPE_DISTANCE_THRESHOLD = 100;
-        private static final int SWIPE_VELOCITY_THRESHOLD = 100;
+        private static final int DISTANCE_THRESHOLD = 100;
+        private static final int VELOCITY_THRESHOLD = 100;
 
         @Override
         public boolean onDown(MotionEvent e) {
@@ -40,11 +40,14 @@ public class OnSwipeTouchListener implements OnTouchListener {
         }
         //Checks for swiping
         @Override
-        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-            float distanceX = e2.getX() - e1.getX();
-            float distanceY = e2.getY() - e1.getY();
-            if (Math.abs(distanceX) > Math.abs(distanceY) && Math.abs(distanceX) > SWIPE_DISTANCE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
-                if (distanceX > 0)
+        public boolean onFling(MotionEvent event1, MotionEvent event2, float velX, float velY) {
+            float disX = event2.getX() - event1.getX();
+            float disY = event2.getY() - event1.getY();
+            //If moving more left and right than up and down AND
+            //if moving more than the minimum distance AND
+            //if moving faster than the minimum velocity, we have a swipe
+            if (Math.abs(disX) > Math.abs(disY) && Math.abs(disX) > DISTANCE_THRESHOLD && Math.abs(velX) > VELOCITY_THRESHOLD) {
+                if (disX > 0) //Postive distance is right, negative is left
                     onSwipeRight();
                 else
                     onSwipeLeft();
